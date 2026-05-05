6c7231985ff31d01be2763e9a5ab89d0ab175f59
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2026-05-05 10:59:10 +0200

    push

--- naemtnu/src/App.vue
+++ naemtnu/src/App.vue
@@ -1,22 +1,9 @@
 <script setup>
-import Header from "@/components/Header.vue";
-import SeoProcess from "@/components/SeoProcess.vue";
-import Soegeord from "@/assets/soegeord.png";
-import Anbefalinger from "@/assets/anbefalinger.png";
-import Faq from "@/components/Faq.vue";
-import kontakt from "@/assets/kontakt.png";
-import Footer from "@/components/Footer.vue";
+
 </script>
 
 <template>
-  <Header />
-  <SeoProcess />
-  <Soegeord />
-  <Anbefalinger />
-  <Faq />
-  <kontakt />
-  <Footer />
-  <RouterView />
+
 </template>
 
 <style scoped></style>
